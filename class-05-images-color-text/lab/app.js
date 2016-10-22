2ef99bf117c35a020cbe50c3465c3bcddd456da5
Alex <ajshort2010@hotmail.com>
2016-10-21 22:49:21 -0700

    Completed 2nd question.

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a,b){ //eslint-disable-line
+function sum(a,b) { //eslint-disable-line
   return ('The sum of ' + a + ' and ' + b + ' is ' + (a + b) + '.');
 };
 
@@ -26,12 +26,12 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a,b){ //eslint-disable-line
-
-}
+function multiply(a,b) { //eslint-disable-line
+  return ('The product of ' + a + ' and ' + b + ' is ' + (a * b) + '.');
+};
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+ testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
