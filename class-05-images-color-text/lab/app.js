1202ea16b75747a7d468a3b2fa17ed0b71870365
Alex <ajshort2010@hotmail.com>
2016-10-23 19:38:08 -0700

    changed back to original.

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -8,14 +8,15 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a,b) { //eslint-disable-line
-  var answer = a + b;
-  return sum[1] = 'The sum of ' + a + ' and ' + b + ' is ' + answer + '.';
-}
+  var sumAnswer = (a + b);
+  var sumText = ('The sum of ' + a + ' and ' + b + ' is ' + (a + b) + '.');
+  return [sumAnswer, sumText];
+};
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
-
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
@@ -28,6 +29,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply( a, b) { //eslint-disable-line
+
   return ('The product of ' + a + ' and ' + b + ' is ' + (a * b) + '.');
 }
 // Here is the test for multiply(); uncomment it to run it
--- class-05-images-color-text/lab/tests.js
+++ class-05-images-color-text/lab/tests.js
@@ -1,7 +1,7 @@
 'use strict';
 
 function testSum() { //eslint-disable-line
-  if (sum(4, 7) === 'The sum of 4 and 7 is 11.') {
+  if (sum(4, 7)[1] === 'The sum of 4 and 7 is 11.') {
     console.log('%c TEST FOR sum() PASSES', 'color: green');
   } else {
     console.log('%c TEST FOR sum() FAILS', 'color: red');
